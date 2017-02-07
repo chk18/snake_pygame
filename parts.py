8c1d96576d6ae4bc6e445de08d1a8f20338c07a6
Chris <me@chriskefalas.me>
2017-02-07 08:24:38 -0500

    First commit!

---
+++ parts.py
@@ -0,0 +1,76 @@
+####################################
+#                                  #
+#              PART ONE            #
+#                                  #
+####################################
+
+# Create variables that define our colors we will need for the game
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 180, 0)
+
+# Define variables that will definte the level width and height
+display_width = 800
+display_height = 600
+
+####################################
+#                                  #
+#              PART TWO            #
+#           LINES 105 - 115        #
+####################################
+
+# Adding the functionality to keep track of the score
+def score(score):
+    text = smallfont.render("Score: " + str(score), True, black)
+    gameDisplay.blit(text, [0,0])
+
+####################################
+#                                  #
+#              PART THREE          #
+#           LINES 155 - 178        #
+####################################
+#   WELCOME SCREEN
+    gameDisplay.fill(white)
+    message_to_screen("Welcome to Slither",
+                      green,
+                      -100,
+                      "large")
+    message_to_screen("The objective of the game is to eat red apples",
+                      black,
+                      -30,
+                      "small")
+    message_to_screen("The more apples you eat, the longer you get",
+                      black,
+                      10,
+                      "small")
+    message_to_screen("If you run into yourself or the edges, you lose!",
+                      black,
+                      50,
+                      "small")
+
+    message_to_screen("Press C to play, P to pause, or Q to quit.",
+                      black,
+                      180,
+                      "small")
+    pygame.display.update()
+    clock.tick(15)
+
+####################################
+#                                  #
+#              PART FOUR           #
+#           LINES 203 - 214        #
+####################################
+
+        while gameOver == True:
+            gameDisplay.fill(white)
+            message_to_screen("Game over",
+                              red,
+                              y_displace = -50,
+                              size = "large")
+
+            message_to_screen("press C to play again or Q to quit",
+                              black,
+                              50,
+                              size = "medium")
+            pygame.display.update()
